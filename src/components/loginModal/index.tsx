92fd30a4e97a10480812dadb865f8f7a6d8b8be5
kimkh05 <nestjs01@naver.com>
2022-10-04 11:43:34 +0900

    fix: 각각 스타일 컴포넌트 생성

--- src/components/loginModal/index.tsx
+++ src/components/loginModal/index.tsx
@@ -1,25 +1,31 @@
 import styled from "styled-components";
 import { ModalBackgroundImg, Google, Facebook, Github } from "../../assets/img";
-import theme from "../../styles/theme";
 
 const LoginModal = () => {
   return (
     <ModalBackground>
       <ModalWrapper>
-        <span>소셜 계정으로 로그인</span>
+        <LoginText>소셜 계정으로 로그인</LoginText>
         <div>
-          <img src={ModalBackgroundImg} alt="모달이미지" />
-          <div>
-            <img src={Google} alt="Google OAuth" />
-            <img src={Github} alt="Github OAuth" />
-            <img src={Facebook} alt="Facebook OAuth" />
-          </div>
-          <span>환영합니다 !</span>
+          <Image src={ModalBackgroundImg} alt="모달이미지" />
+          <OAuthImgWrapper>
+            <Image src={Google} alt="Google OAuth" />
+            <Image src={Github} alt="Github OAuth" />
+            <Image src={Facebook} alt="Facebook OAuth" />
+          </OAuthImgWrapper>
+          <WelcomeText>환영합니다 !</WelcomeText>
         </div>
       </ModalWrapper>
     </ModalBackground>
   );
 };
+
+const LoginText = styled.span`
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 44px;
+  padding: 5% 0 5% 0;
+`;
 
 const ModalBackground = styled.div`
   height: 100vh;
@@ -27,6 +33,30 @@ const ModalBackground = styled.div`
   place-content: center;
   background-color: rgba(0, 0, 0, 0.5);
   z-index: 1;
+`;
+
+const Image = styled.img`
+  height: max-content;
+  margin-top: 20px;
+  border-radius: 0 0 10px 10px;
+`;
+
+const OAuthImgWrapper = styled.div`
+  width: 500px;
+  display: flex;
+  justify-content: space-around;
+  position: absolute;
+  top: 45%;
+`;
+
+const WelcomeText = styled.span`
+  color: #ffffff;
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 44px;
+  position: absolute;
+  top: 65%;
+  left: 45%;
 `;
 
 const ModalWrapper = styled.div`
@@ -39,33 +69,6 @@ const ModalWrapper = styled.div`
   justify-content: space-between;
   border: 1px solid black;
   border-radius: 10px;
-  & > span {
-    font-weight: 700;
-    font-size: 32px;
-    line-height: 44px;
-    padding: 5% 0 5% 0;
-  }
-  & > div > img{
-    height: max-content;
-    margin-top: 20px;
-    border-radius: 0 0 10px 10px;
-  }
-  & > div > div {
-    width: 500px;
-    display: flex;
-    justify-content: space-around;
-    position: absolute;
-    top: 45%;
-  }
-  & > div > span {
-    color: #ffffff;
-    font-size: 32px;
-    font-weight: 700;
-    line-height: 44px;
-    position: absolute;
-    top: 65%;
-    left: 45%;
-  }
 `;
 
 export default LoginModal;
