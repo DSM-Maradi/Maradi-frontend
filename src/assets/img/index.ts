5c04fbe46e058d5d77b33dcb7d90da6f500d4144
kimkh05 <nestjs01@naver.com>
2022-10-04 11:17:45 +0900

    feat: 이미지 추가

---
+++ src/assets/img/index.ts
@@ -0,0 +1,4 @@
+export { default as ModalBackgroundImg } from "./modalBackground.svg";
+export { default as Google } from "./google.svg";
+export { default as Github } from "./github.svg";
+export { default as Facebook } from "./facebook.svg";
