2f3d474fe11b9f9818d2ccc5343d540a760b824a
Starving._.Minsung <kms73033222@gmail.com>
2022-10-05 15:12:02 +0900

    feat: 이미지 추가

---
+++ src/assets/img/index.ts
@@ -0,0 +1,4 @@
+export { default as mainImg } from "./mainImg.svg";
+export { default as headLogo } from "./headLogo.svg";
+export { default as searchFont } from "./searchFont.svg";
+export { default as listImg } from "./listImg.svg";
