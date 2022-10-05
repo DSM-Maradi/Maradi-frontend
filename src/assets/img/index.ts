1462ce2f413c7c162063681be74b1aa6fa1a763f
kimkh05 <nestjs01@naver.com>
2022-10-05 21:27:57 +0900

    feat: 이미지 추가 및 삭제

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,8 +1,9 @@
-export { default as mainImg } from "./mainImg.svg";
-export { default as headLogo } from "./headLogo.svg";
-export { default as searchFont } from "./searchFont.svg";
-export { default as listImg } from "./listImg.svg";
+export { default as MainImg } from "./mainImg.png";
+export { default as HeadLogo } from "./headLogo.svg";
+export { default as SearchFont } from "./searchFont.svg";
+export { default as ListImg } from "./listImg.svg";
 export { default as ModalBackgroundImg } from "./modalBackground.svg";
 export { default as Google } from "./google.svg";
 export { default as Github } from "./github.svg";
 export { default as Facebook } from "./facebook.svg";
+export { default as SortArrow } from "./sortarrow.svg";
