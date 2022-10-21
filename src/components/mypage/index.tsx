b393b1b93f004a66ffaf27f9affe70effce9fe94
kimkh05 <nestjs01@naver.com>
2022-10-21 18:38:29 +0900

    feat: 마이페이지 퍼블리싱

---
+++ src/components/mypage/index.tsx
@@ -0,0 +1,62 @@
+import Header from "../common/header";
+import styled from "styled-components";
+import { ProjectList } from "../../constance/projectlist";
+import ProjectCard from "./projectcard";
+import { useState } from "react";
+
+const MyPage = () => {
+  const [checked, setChecked] = useState<number>(-1);
+  return (
+    <Wrapper>
+      <Header />
+      <ProjectWrapper>
+        <Text>나의 프로젝트</Text>
+        <ListWrapper>
+          {ProjectList.map((e, index) => {
+            return (
+              <ProjectCard
+                key={e.id}
+                index={index}
+                title={e.title}
+                text={e.text}
+                date={e.date}
+                goalMoney={e.goalMoney}
+                nowMoney={e.nowMoney}
+                checked={checked}
+                setChecked={setChecked}
+              />
+            );
+          })}
+        </ListWrapper>
+      </ProjectWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProjectWrapper = styled.div`
+  margin-top: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Text = styled.span`
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 44px;
+  margin-bottom: 80px;
+`;
+
+const ListWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export default MyPage;
---
+++ src/components/mypage/projectcard/index.tsx
@@ -0,0 +1,147 @@
+import styled from "styled-components";
+import { ThreeDot, ListImg } from "../../../assets/img";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+interface PropsType {
+  index: number;
+  title: string;
+  text: string;
+  date: string;
+  goalMoney: number;
+  nowMoney: number;
+  checked: number;
+  setChecked: (checked: number) => void;
+}
+
+const ProjectCard = ({
+  index,
+  title,
+  text,
+  date,
+  goalMoney,
+  nowMoney,
+  checked,
+  setChecked,
+}: PropsType) => {
+  const [initial, setInitial] = useState<boolean>(false);
+  return (
+    <ListItems>
+      <ImgWrapper>
+        <Image
+          src={ThreeDot}
+          onClick={() => {
+            index !== checked ? setChecked(index) : setChecked(-1);
+            setInitial(!initial);
+          }}
+          alt="프로젝트 수정 및 삭제"
+        />
+        {checked === index && initial && (
+          <SmallList>
+            <ListLink to="">
+              <List>수정</List>
+            </ListLink>
+            <ListLink to="">
+              <List>삭제</List>
+            </ListLink>
+          </SmallList>
+        )}
+      </ImgWrapper>
+      <TitleWrapper>
+        <h3>{title}</h3>
+        <ListContents>{text}</ListContents>
+      </TitleWrapper>
+      <ListBottom>
+        <RegisterDate>등록날짜 | {date}</RegisterDate>
+        <Money>
+          {goalMoney}원 / {nowMoney}원
+        </Money>
+      </ListBottom>
+    </ListItems>
+  );
+};
+
+const TitleWrapper = styled.div`
+  padding: 0 20px 0 20px;
+`;
+
+const ListItems = styled.li`
+  width: 500px;
+  height: 400px;
+  margin: 0 40px 5% 0;
+  border-radius: 12px;
+  box-shadow: 3px 3px 5px 3px #b6b6b6;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ListContents = styled.div`
+  color: ${({ theme }) => theme.color.gray900};
+  margin-top: 10px;
+`;
+const ListBottom = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 10px;
+`;
+
+const RegisterDate = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+  font-family: ${({ theme }) => theme.font.arita};
+  line-height: 14px;
+  color: ${({ theme }) => theme.color.gray700};
+  display: flex;
+  flex-direction: column-reverse;
+`;
+
+const Money = styled.span`
+  color: ${({ theme }) => theme.color.darkmain};
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 24px;
+  font-family: ${({ theme }) => theme.font.arita};
+`;
+
+const List = styled.div`
+  width: 140px;
+  height: 45px;
+  color: ${({ theme }) => theme.color.black};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.color.gray700};
+`;
+
+const ListLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const SmallList = styled.div`
+  width: 144px;
+  height: 90px;
+  position: absolute;
+  transform: translate(100px, 50px);
+  border-radius: 10px;
+  background: ${({ theme }) => theme.color.white};
+`;
+
+const Image = styled.img`
+  width: 30px;
+  height: 30px;
+  margin: 15px 15px 0 0;
+  cursor: pointer;
+`;
+
+const ImgWrapper = styled.div`
+  width: 100%;
+  height: 150px;
+  background-image: url(${ListImg});
+  display: flex;
+  justify-content: flex-end;
+  overflow: hidden;
+`;
+
+export default ProjectCard;
