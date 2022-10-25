fd0598a0f94bc50bf007160363a4f3f49bdd5cd1
Starving._.Minsung <kms73033222@gmail.com>
2022-10-25 23:46:26 +0900

    fix: 요구조건 수정

--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -1,14 +1,7 @@
 import styled from "styled-components";
 import { Arrow } from "../../assets/img";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import ProjectSubmitModal from "../projectsubmitmodal";
-
-const Upload = () => {
-  const [showModal, setShowModal] = useState(false);
-  const activeModal = () => {
-    setShowModal((open) => !open);
-  };
-};
 
 function CreateProject() {
   const [input, setInputs] = useState({
@@ -19,7 +12,7 @@ function CreateProject() {
 
   const { title, content } = input;
 
-  const onChange = (e: any) => {
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { value, name } = e.target;
     setInputs({
       ...input,
@@ -27,50 +20,38 @@ function CreateProject() {
     });
   };
 
-  const onSave = () => {
-    //나중에 구현...
-  };
-
-  const onUpload = () => {
-    //나중에 구현...
-  };
-
-  const revertPage = () => {
-    //나중에 구현...
-  };
-
   return (
     <>
       <MainContainer>
+        {modal && <ProjectSubmitModal setModal={setModal} />}
         <WriteContainer>
-          {modal && <ProjectSubmitModal setModal={setModal} />}
-          <input
+          <WriteTitle
             placeholder="제목을 입력해주세요"
             onChange={onChange}
             name="title"
             value={title}
           />
-          <textarea
+          <WriteContents
             onChange={onChange}
             placeholder="내용을 입력해주세요"
             name="content"
             value={content}
           />
           <InterfaceBar>
-            <img src={Arrow} />
-            <p>뒤로가기</p>
-            <button id="saveButton">
-              <span>임시저장</span>
-            </button>
-            <button onClick={() => setModal(true)} id="upLoadButton">
-              <span>업로드 하기</span>
-            </button>
+            <GoBackImg src={Arrow} />
+            <GoBackFont>뒤로가기</GoBackFont>
+            <SaveButton id="">
+              <ButtonSpan>임시저장</ButtonSpan>
+            </SaveButton>
+            <UploadBtn onClick={() => setModal(true)} id="upLoadButton">
+              <ButtonSpan>업로드 하기</ButtonSpan>
+            </UploadBtn>
           </InterfaceBar>
         </WriteContainer>
         <PostWriteContainer>
           <PostWrite>
-            <h1>{title}</h1>
-            <span>{content}</span>
+            <PostWriteTitle>{title}</PostWriteTitle>
+            <PostWriteContent>{content}</PostWriteContent>
           </PostWrite>
         </PostWriteContainer>
       </MainContainer>
@@ -78,7 +59,7 @@ function CreateProject() {
   );
 }
 
-const MainContainer = styled.div`
+const MainContainer = styled.form`
   display: flex;
   width: 100vw;
   height: 100vh;
@@ -88,38 +69,39 @@ const WriteContainer = styled.div`
   width: 50%;
   height: 100%;
   position: relative;
-  input {
-    width: 591px;
-    display: block;
-    margin: 76px 0px 55px 61px;
-    font-size: 30px;
-    ::placeholder {
-      font-family: "Noto Sans";
-      font-style: normal;
-      color: #b6b6b6;
-    }
-  }
-  textarea {
-    width: 591px;
-    height: 500px;
-    display: block;
-    border: none;
-    resize: none;
-    font-size: 20px;
-    margin-left: 61px;
-    outline: none;
-    ::placeholder {
-      font-size: 20px;
-      font-family: "Noto Sans";
-      font-style: normal;
-      color: #b6b6b6;
-    }
+`;
+
+const WriteTitle = styled.input`
+  width: 591px;
+  display: block;
+  margin: 76px 0px 55px 61px;
+  font-size: 30px;
+  ::placeholder {
+    font-family: ${({ theme }) => theme.font.noto};
+    color: ${({ theme }) => theme.color.gray700};
   }
 `;
+
+const WriteContents = styled.textarea`
+  width: 591px;
+  height: 500px;
+  display: block;
+  border: none;
+  resize: none;
+  font-size: 20px;
+  margin-left: 61px;
+  outline: none;
+  ::placeholder {
+    font-size: 20px;
+    font-family: ${({ theme }) => theme.font.noto};
+    color: ${({ theme }) => theme.color.gray700};
+  }
+`;
+
 const PostWriteContainer = styled.div`
   width: 50%;
   height: 100%;
-  background-color: #fbfbfb;
+  background-color: ${({ theme }) => theme.color.gray100};
   display: flex;
   align-items: center;
   justify-content: center;
@@ -128,65 +110,62 @@ const PostWriteContainer = styled.div`
 const PostWrite = styled.div`
   width: 80%;
   height: 90%;
-  h1 {
-    margin-bottom: 10px;
-  }
-  span {
-    font-size: 30px;
-    word-break: break-all;
-  }
-
   overflow-y: auto;
+`;
+
+const PostWriteTitle = styled.h1`
+  margin-bottom: 10px;
+`;
+
+const PostWriteContent = styled.span`
+  font-size: 30px;
+  word-break: break-all;
 `;
 
 const InterfaceBar = styled.div`
   position: absolute;
   width: 100%;
   height: 74px;
-  background: #ffffff;
+  background: ${({ theme }) => theme.color.white};
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
   display: flex;
   align-items: center;
-
   bottom: 0;
-  p {
-    padding-top: 4px;
-    padding-left: 5px;
-    width: 84px;
-    height: 24px;
-    cursor: pointer;
-  }
-  #saveButton {
-    margin-left: 310px;
-    padding: 14px 18px;
-    width: 92px;
-    height: 48px;
-    background: #3eaf0e;
-    border-radius: 5px;
-    justify-content: center;
-    cursor: pointer;
-    span {
-      color: #ffffff;
-      font-size: 15px;
-    }
-  }
-  #upLoadButton {
-    margin-left: 40px;
-    padding: 14px 18px;
-    width: 106px;
-    height: 48px;
-    background: #3eaf0e;
-    border-radius: 5px;
-    cursor: pointer;
-    span {
-      color: #ffffff;
-      font-size: 15px;
-    }
-  }
-  img {
-    margin-left: 40px;
-    cursor: pointer;
-  }
+`;
+
+const SaveButton = styled.button`
+  margin-left: 350px;
+  padding: 14px 18px;
+  width: 92px;
+  height: 48px;
+  background: ${({ theme }) => theme.color.main};
+  border-radius: 5px;
+  justify-content: center;
+  cursor: pointer;
+`;
+const ButtonSpan = styled.span`
+  color: ${({ theme }) => theme.color.white};
+  font-size: 15px;
+`;
+
+const GoBackFont = styled.p`
+  margin-left: 10px;
+  cursor: pointer;
+`;
+
+const GoBackImg = styled.img`
+  margin-left: 20px;
+  cursor: pointer;
+`;
+
+const UploadBtn = styled.div`
+  margin-left: 40px;
+  padding: 14px 18px;
+  width: 106px;
+  height: 48px;
+  background: ${({ theme }) => theme.color.main};
+  border-radius: 5px;
+  cursor: pointer;
 `;
 
 export default CreateProject;
