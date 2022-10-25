4a2e03f21575bd33ebf365b693a0aee5d49f1124
Starving._.Minsung <kms73033222@gmail.com>
2022-10-25 23:46:45 +0900

    fix: 요구조건 수정

--- src/components/projectsubmitmodal/index.tsx
+++ src/components/projectsubmitmodal/index.tsx
@@ -9,35 +9,79 @@ const ProjectSubmitModal = ({ setModal }: PropsType) => {
   return (
     <ModalContainer>
       <ModalWrapper>
-        <img src={XButton} onClick={() => setModal(false)}></img>
+        <XBtn src={XButton} onClick={() => setModal(false)}></XBtn>
         <ImgBlock>
-          <ImgUploadContainer>
-            <img src={ImgUpload} />
-            <p>이미지를 업로드 해주세요</p>
+          <ImgUploadContainer htmlFor="input-file">
+            <UploadImg src={ImgUpload} />
+            <UploadFont>이미지를 업로드 해주세요</UploadFont>
+            <FileSelector
+              type="file"
+              id="input-file"
+              accept="image/jpg, image/png, image/jpeg"
+            />
           </ImgUploadContainer>
-          <textarea placeholder="간단한 설명을 작성해주세요." />
+
+          <UploadTextarea placeholder="간단한 설명을 작성해주세요." />
         </ImgBlock>
-        <button>
-          <span onClick={() => setModal(false)}>제출하기</span>
-        </button>
+        <PostForm onClick={() => setModal(false)}>
+          <PostFormSpan>제출하기</PostFormSpan>
+        </PostForm>
       </ModalWrapper>
     </ModalContainer>
   );
 };
 
+const PostForm = styled.form`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 14px 18px;
+  margin: 10px;
+
+  width: 92px;
+  height: 48px;
+  background: ${({ theme }) => theme.color.main};
+  border-radius: 5px;
+  justify-content: center;
+  cursor: pointer;
+
+  float: right;
+`;
+
+const PostFormSpan = styled.span`
+  color: ${({ theme }) => theme.color.white};
+  font-size: 15px;
+`;
+
+const XBtn = styled.img`
+  float: right;
+  cursor: pointer;
+  width: 15px;
+  height: 15px;
+  margin: 10px;
+`;
+
+const FileSelector = styled.input`
+  width: 165px;
+  height: 120px;
+  display: none;
+`;
+
 const ImgBlock = styled.div`
   display: flex;
   margin-top: 60px;
-  textarea {
-    width: 297px;
-    height: 120px;
+`;
+const UploadTextarea = styled.textarea`
+  width: 297px;
+  height: 120px;
+  font-size: 12px;
+  border-radius: 8px;
+  resize: none;
+  padding: 20px 20px;
+  margin-left: 20px;
+  background-color: ${({ theme }) => theme.color.gray100};
+  ::placeholder {
     font-size: 12px;
-    padding: 20px 20px;
-    margin-left: 20px;
-    background-color: #fbfbfb;
-    ::placeholder {
-      font-size: 12px;
-    }
   }
 `;
 
@@ -57,53 +101,36 @@ const ModalWrapper = styled.div`
   width: 550px;
   height: 300px;
   text-align: center;
-  background-color: white;
+  background-color: ${({ theme }) => theme.color.white};
   align-items: center;
   z-index: 3;
 
-  button {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    padding: 14px 18px;
-    width: 92px;
-    height: 48px;
-    background: #3eaf0e;
-    border-radius: 5px;
-    justify-content: center;
-    cursor: pointer;
-
-    float: right;
-    span {
-      color: #ffffff;
-      font-size: 15px;
-    }
-  }
-  img {
-    float: right;
-    cursor: pointer;
-    width: 15px;
-    height: 15px;
+  span {
+    color: ${({ theme }) => theme.color.white};
+    font-size: 15px;
   }
 `;
-const ImgUploadContainer = styled.div`
+
+const ImgUploadContainer = styled.label`
   display: flex;
   width: 165px;
   height: 120px;
+  cursor: pointer;
   margin-left: 30px;
   border-radius: 3.75px;
   flex-direction: column;
   justify-content: center;
-  background-color: #f0f0f0;
+  background-color: ${({ theme }) => theme.color.gray300};
   justify-content: center;
   align-items: center;
-  img {
-    margin-bottom: 10px;
-  }
-  p {
-    color: #b6b6b6;
-    font-size: 7.5px;
-  }
+`;
+
+const UploadImg = styled.img`
+  margin-bottom: 10px;
+`;
+const UploadFont = styled.p`
+  color: ${({ theme }) => theme.color.gray700};
+  font-size: 7.5px;
 `;
 
 export default ProjectSubmitModal;
