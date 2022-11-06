a18f8e790a13b6ccbbeb08de48667f847bdf00b8
kimkh05 <nestjs01@naver.com>
2022-11-06 13:50:50 +0900

    fix: 디자인 오류 수정

--- src/components/common/header/nameList/index.tsx
+++ src/components/common/header/nameList/index.tsx
@@ -33,7 +33,7 @@ const ListWrapper = styled.div`
   display: flex;
   flex-direction: column;
   position: absolute;
-  transform: translate(300px, 30px);
+  transform: translate(300px, 90px);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 8px;
   z-index: 4;
