53875d4fc3d79f7e1abd459a112dcc16038e6a06
kimkh05 <nestjs01@naver.com>
2022-11-06 13:51:00 +0900

    feat: 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Main from "./components/main";
 import CreateProject from "./components/createproject";
 import MyPage from "./components/mypage";
+import Hall from "./components/hall";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path="/" element={<Main />} />
           <Route path="/project/createProject" element={<CreateProject />} />
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/hall" element={<Hall />} />
         </Routes>
       </BrowserRouter>
     </StyleProvider>
