f859ddac0bfd8b5946ee36fe530f5ca408fbc961
kimkh05 <nestjs01@naver.com>
2022-11-06 13:49:42 +0900

    feat: 더미데이터 추가

---
+++ src/constance/hall/index.ts
@@ -0,0 +1,23 @@
+interface DummyType {
+  id: number;
+  name: string;
+  amount: number;
+}
+
+export const DummyData: DummyType[] = [
+  {
+    id: 1,
+    name: "이준서",
+    amount: 100000,
+  },
+  {
+    id: 2,
+    name: "오정수",
+    amount: 40000,
+  },
+  {
+    id: 3,
+    name: "황진성",
+    amount: 1000,
+  },
+];
