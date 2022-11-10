b5648ac9cca417b094b7952bc51591ad0ad876dd
kimkh05 <nestjs01@naver.com>
2022-11-10 12:21:29 +0900

    fix: 비동기 처리로 변경

--- src/apis/project/create.ts
+++ src/apis/project/create.ts
@@ -1,22 +1,24 @@
 import instance from "../axios";
 
-function projectCreate(
+export default async function projectCreate(
   title: string,
   content: string,
   image_url: string,
   image_description: string
 ) {
-  const response = instance.post("/project", {
-    title: title,
-    content: content,
-    image_url: image_url,
-    image_description: image_description,
-  }, {
-    headers: {
-        Authorization: "Bearer " + localStorage,
+  const response = await instance.post(
+    "/project",
+    {
+      title: title,
+      content: content,
+      image_url: image_url,
+      image_description: image_description,
+    },
+    {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("access_token"),
+      },
     }
-  });
+  );
   return response;
 }
-
-export default projectCreate;
