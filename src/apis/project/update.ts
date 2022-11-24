1d61594df61b6fce38d9bdbbd8d2e0a856c7ed7e
kimkh05 <nestjs01@naver.com>
2022-11-24 23:14:10 +0900

    feat: 프로젝트 수정 api 연동

--- src/apis/project/update.ts
+++ src/apis/project/update.ts
@@ -1,1 +1,15 @@
-export {};
+import instance from "../axios";
+
+export const updateProject = async (
+  project_id: number,
+  name: string,
+  content: string,
+  image_url: string
+) => {
+  await instance.put(`/project/${project_id}`, {
+    title: name,
+    content: content,
+    image_url: image_url,
+    image_description: "",
+  });
+};
